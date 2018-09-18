2540e0c6d244205877aa31e670589b875e1f74af
johan <johan@bareksa.com>
2018-09-18 22:16:13 +0700

    Fix commit 4

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -56,7 +56,7 @@ class User_model extends CI_Model
         $this->db->select('*');
         $this->db->from('posting');
         $this->db->where("posting.post_id", $idPosting);
-        $this->db->join('user', 'user.user_id = posting.user_id');
+        //  $this->db->join('user', 'user.user_id = posting.user_id');
         $this->db->join('komen', 'komen.post_id = posting.post_id');
         $this->db->order_by("posting.waktu", "desc");
         // $this->db->where('posting.id_user',1);
