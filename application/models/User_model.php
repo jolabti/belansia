1760ba253b2409d22212c4a8c93691aca1cdab06
johan <johan@bareksa.com>
2018-09-18 20:28:00 +0700

    Fix commit 2

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -5,146 +5,152 @@ if (!defined('BASEPATH')) {
     exit('No direct script access allowed');
 }
 
-class User_model extends CI_Model {
+class User_model extends CI_Model
+{
 
 
     // ========================== Posting ============================
     // Get Posting
-    public function get_posting(){
-     /*
-     $this->db->select('*');
-        $this->db->from('komen');
-        $this->db->join('user', 'user.user_id = komen.user_id');
-     */
+    public function get_posting()
+    {
+     
+
+        //
+        //  $this->db->select('*');
+        //  $this->db->from('posting');
+        //  $this->db->join('user', 'user.user_id = posting.user_id');
+        // //  $this->db->join('komen', 'komen.user_id = posting.user_id');
+        // //  $this->db->join('komen', 'komen.post_id = posting.post_id');
+        //  $this->db->order_by("posting.waktu", "desc");
+        // 		// $this->db->where('posting.id_user',1);
+        // 		// $this->db->limit(5);
+        //     $q = $this->db->get();
+        //     return $q->result();
 
 
-    	   $this->db->select('*');
-         $this->db->from('posting');
-         $this->db->join('user', 'user.user_id = posting.user_id');
+
+
+
+        $this->db->select('*');
+        $this->db->from('posting');
+        $this->db->join('user', 'user.user_id = posting.user_id');
         //  $this->db->join('komen', 'komen.user_id = posting.user_id');
         //  $this->db->join('komen', 'komen.post_id = posting.post_id');
-				 $this->db->order_by("posting.waktu", "desc");
-						// $this->db->where('posting.id_user',1);
-						// $this->db->limit(5);
-            $q = $this->db->get();
-            return $q->result();
+        $this->db->order_by("posting.waktu", "asc");
+        // $this->db->where('posting.id_user',1);
+        // $this->db->limit(5);
+        $q = $this->db->get();
+        return $q->result();
     }
 
 
-  public function get_komentar($idPosting){
-     /*
-     $this->db->select('*');
-        $this->db->from('komen');
-        $this->db->join('user', 'user.user_id = komen.user_id');
-     */
+    public function get_komentar($idPosting)
+    {
+        /*
+        $this->db->select('*');
+           $this->db->from('komen');
+           $this->db->join('user', 'user.user_id = komen.user_id');
+        */
 
 
-         $this->db->select('*');
-         $this->db->from('posting');
-         $this->db->where("posting.post_id",$idPosting);
-         $this->db->join('user', 'user.user_id = posting.user_id');
-         $this->db->join('komen', 'komen.post_id = posting.post_id');
-         $this->db->order_by("posting.waktu", "desc");
-            // $this->db->where('posting.id_user',1);
-            // $this->db->limit(5);
-            $q = $this->db->get();
-            return $q->result();
+        $this->db->select('*');
+        $this->db->from('posting');
+        $this->db->where("posting.post_id", $idPosting);
+        $this->db->join('user', 'user.user_id = posting.user_id');
+        $this->db->join('komen', 'komen.post_id = posting.post_id');
+        $this->db->order_by("posting.waktu", "desc");
+        // $this->db->where('posting.id_user',1);
+        // $this->db->limit(5);
+        $q = $this->db->get();
+        return $q->result();
     }
 
-    public function get_sample_posting(){
-
-          $q = $this->db->get('posting');
-          return $q->result();
-
+    public function get_sample_posting()
+    {
+        $q = $this->db->get('posting');
+        return $q->result();
     }
     // ============================ Insert Komentar =======================
-    public function insert_komen($data=array()){
-        $this->db->insert('komen',$data);
-
+    public function insert_komen($data=array())
+    {
+        $this->db->insert('komen', $data);
     }
 
     // ============================ Insert Posting ========================
     // Insert Posting
-    public function insert_posting($data=array()){
-    	$this->db->insert('posting',$data);
+    public function insert_posting($data=array())
+    {
+        $this->db->insert('posting', $data);
     }
 
-    public function masukkin_komentar($data=array()){
-
-          $this->db->insert('komen',$data);
-
+    public function masukkin_komentar($data=array())
+    {
+        $this->db->insert('komen', $data);
     }
 
 
     // =========================== Delete User ============================
     // Delete user
-  	public function delete_posting($data=array()){
-  		$this->db->delete('delete',$data);
-  	}
+    public function delete_posting($data=array())
+    {
+        $this->db->delete('delete', $data);
+    }
 
-  	// ============================ Komentar ==============================
+    // ============================ Komentar ==============================
     // Komentar
-    public function comment(){
+    public function comment()
+    {
         $this->db->select('*');
         $this->db->form('komen');
         $this->db->join('user', 'user.user_id = komen.user_id');
-                    $this->db->order_by("komen.waktu", "desc");
-                    $this->db->where('komen.user_id',2);
-                    $this->db->limits(7);
-            $q = $this->db->get();
-            return $q->result();
-
+        $this->db->order_by("komen.waktu", "desc");
+        $this->db->where('komen.user_id', 2);
+        $this->db->limits(7);
+        $q = $this->db->get();
+        return $q->result();
     }
 
-    public function insert_detail_komentar($data=array()){
-
-        $this->db->insert('komen',$data);
+    public function insert_detail_komentar($data=array())
+    {
+        $this->db->insert('komen', $data);
     }
-    
-    
-      public function update_user($iduser){
 
-          $kota = $this->input->post("user_kota");
-          $umur = $this->input->post("user_umur");
 
-              $gambar = "";
-              $data = array(
+    public function update_user($iduser)
+    {
+        $kota = $this->input->post("user_kota");
+        $umur = $this->input->post("user_umur");
+
+        $gambar = "";
+        $data = array(
                     'urlgambar' => '',
                     'kota' => $kota,
                     'umur' => $umur
 
                     );
 
-              $this->db->where('user_id', $iduser);
-              $this->db->update('user', $data);
+        $this->db->where('user_id', $iduser);
+        $this->db->update('user', $data);
 
-          if($kota != NULL || $umur!=NULL){
-
+        if ($kota != null || $umur!=null) {
             $param['status']='oke';
-
-          }
-
-          else {
-
+        } else {
             $param['status']='failed';
+        }
 
-          }
-
-          echo json_encode($param);
+        echo json_encode($param);
     }
 
 
 
 
-    public function get_user($iduser){
-
-      $this->db->select('*');
-      $this->db->from('user');
-      $this->db->where('user_id',$iduser);
-       $q = $this->db->get();
-       return $q->row();
-
-
+    public function get_user($iduser)
+    {
+        $this->db->select('*');
+        $this->db->from('user');
+        $this->db->where('user_id', $iduser);
+        $q = $this->db->get();
+        return $q->row();
     }
 
 
@@ -156,5 +162,5 @@ class User_model extends CI_Model {
 
 
 
-// Penutup
+    // Penutup
 }
